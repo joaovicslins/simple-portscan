38ef7b3c1d2aa82e7a1c755709659d0a36351a2a
joaovicslins <joaovic.slins@gmail.com>
2020-09-21 10:00:48 -0300

    Timeout insertion

--- simple_scan.py
+++ simple_scan.py
@@ -16,20 +16,21 @@ t1 = datetime.now()
 
 def scan():
     port = 0
-    for port in range(0, 1000):
+    for port in range(0, 8082):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #it create sock stream
+        sock.settimeout(0.1)
         result = sock.connect_ex((ip, port))
         if result == 0:
             #if a socket is listening it will print out the port number
             print(f'\n Porta {port} está aberta.')
             sock.close()
-        else:
-            print(f'\n Porta {port} está fechada. :-(')
-        port += 1      
+        #else:
+            #print(f'\n Porta {port} está fechada. :-(')
+        port += 1
 
 
 scan()
-    
+
 t2 = datetime.now()
 #Total time of scan
 print(f'O tempo total do scan foi: {t2 - t1}')
